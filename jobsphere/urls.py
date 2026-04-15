94c66fe5edb74d9284898b237033f5fb8d3ffc56
Sharmin-Sultana-Shila <sharminsultanashila04@gmail.com>
2026-04-15 22:25:53 +0600

    Added system structure

---
+++ jobsphere/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('login')),
+    path('users/', include('users.urls')),
+    path('recruitments/', include('recruitments.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
